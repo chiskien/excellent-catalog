e1abd1a8022692c52606e37ad4244edfa45550f2
ChisKien <chiskien03214@gmail.com>
2023-08-08 22:03:09 +0700

    Adding Validation for domain entities

--- src/main/java/com/polarbookshop/catalogservice/models/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/models/Book.java
@@ -1,4 +1,24 @@
 package com.polarbookshop.catalogservice.models;
 
-public record Book(String isbn, String title, String author, Double price) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+public record Book(
+        @NotBlank(message = "The Book ISBN must be defined")
+        @Pattern(
+                regexp = "^([0-9]{10}|[0-9]{13}$)",
+                message = "The ISBN format must be valid"
+        )
+        String isbn,
+
+        @NotBlank(message = "The book title must be defined")
+        String title,
+
+        @NotBlank(message = "The author must be defined")
+        String author,
+
+        @NotBlank(message = "The book title must be defined")
+        @Positive(message = "The book price must be greater than zero")
+        Double price) {
 }
