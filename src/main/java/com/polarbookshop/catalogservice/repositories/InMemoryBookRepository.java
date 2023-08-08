b48a37caa08c180c28f31a12f40b899272737edf
ChisKien <chiskien03214@gmail.com>
2023-08-08 21:45:48 +0700

    Custom exceptions

--- src/main/java/com/polarbookshop/catalogservice/repositories/InMemoryBookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/repositories/InMemoryBookRepository.java
@@ -9,30 +9,31 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryBookRepository implements BookRepository {
-    private static final Map<String, Book> bookDictionary = new ConcurrentHashMap<>();
+    private static final Map<String, Book> bookMap = new ConcurrentHashMap<>();
 
     @Override
     public Iterable<Book> findAll() {
-        return bookDictionary.values();
+        return bookMap.values();
     }
 
     @Override
     public Optional<Book> findByIsbn(String isbn) {
-        return Optional.empty();
+        return existByIsbn(isbn) ? Optional.of(bookMap.get(isbn)) : Optional.empty();
     }
 
     @Override
     public boolean existByIsbn(String isbn) {
-        return false;
+        return bookMap.get(isbn) != null;
     }
 
     @Override
     public Book save(Book book) {
-        return null;
+        bookMap.put(book.isbn(), book);
+        return book;
     }
 
     @Override
     public void deleteByIsbn(String isbn) {
-
+        bookMap.remove(isbn);
     }
 }
--- src/main/java/com/polarbookshop/catalogservice/services/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/services/BookService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.catalogservice.services;
 
+import com.polarbookshop.catalogservice.exceptions.BookAlreadyExistException;
+import com.polarbookshop.catalogservice.exceptions.BookNotFoundException;
 import com.polarbookshop.catalogservice.models.Book;
 import com.polarbookshop.catalogservice.repositories.BookRepository;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ public class BookService {
         return bookRepository.findByIsbn(isbn)
                 .orElseThrow(() -> new BookNotFoundException(isbn));
     }
+
     public Book addBooktoCatalog(Book book) {
         if (bookRepository.existByIsbn(book.isbn())) {
             throw new BookAlreadyExistException(book.isbn());
@@ -27,4 +30,20 @@ public class BookService {
         return bookRepository.save(book);
     }
 
+    public void removeBookFromCatalog(String isbn) {
+        bookRepository.deleteByIsbn(isbn);
+    }
+
+    public Book editBook(String isbn, Book book) {
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    Book bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.title(),
+                            book.author(),
+                            book.price()
+                    );
+                    return bookRepository.save(bookToUpdate);
+                }).orElseGet(() -> addBooktoCatalog(book));
+    }
 }
