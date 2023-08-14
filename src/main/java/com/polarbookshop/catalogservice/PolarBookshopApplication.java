410312b3f945be8ec059445f5b107c0bb194bad6
ChisKien <chiskien03214@gmail.com>
2023-08-14 20:12:05 +0700

    clean

--- src/main/java/com/polarbookshop/catalogservice/PolarBookshopApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/PolarBookshopApplication.java
@@ -3,9 +3,8 @@ package com.polarbookshop.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  
 public class PolarBookshopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PolarBookshopApplication.class, args);
     }
