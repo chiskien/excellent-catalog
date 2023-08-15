926910dcc6d06786039a24c3f6c6dca0954b96dc
ChisKien <chiskien03214@gmail.com>
2023-08-15 15:48:49 +0700

    Testing Profiles as Feature Flags

--- src/main/java/com/polarbookshop/catalogservice/exceptions/BookAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.catalogservice.exceptions;
-
-public class BookAlreadyExistException extends RuntimeException {
-    public BookAlreadyExistException(String isbn) {
-        super("A book with ISBN: " + isbn + " already exists.");
-    }
-}
