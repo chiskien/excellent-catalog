1b802c374c688b064539ea73246b29e8fcc15d8d
ChisKien <chiskien03214@gmail.com>
2023-08-17 14:42:12 +0700

    Adding JDBC Audit and CreateDate field in Book.java

--- src/main/java/com/excellentbookshop/catalogservice/models/Book.java
+++ src/main/java/com/excellentbookshop/catalogservice/models/Book.java
@@ -3,13 +3,23 @@ package com.excellentbookshop.catalogservice.models;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
+
+import java.time.Instant;
 
 
 //
 public record Book(
         @Id Long id,
+
+        @CreatedDate
+        Instant createDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
         @NotBlank(message = "The Book ISBN must be defined")
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13}$)",
                 message = "The ISBN format must be valid")
@@ -22,6 +32,7 @@ public record Book(
         Double price,
         @Version int version) {
     public static Book of(String isbn, String title, String author, Double price) {
-        return new Book(null, isbn, title, author, price, 0);
+        return new Book(null, null, null,
+                isbn, title, author, price, 0);
     }
 }
