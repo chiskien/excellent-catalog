9c0af04460b2855c8a9eddc2a3b327533d533c02
ChisKien <chiskien03214@gmail.com>
2023-08-18 10:12:21 +0700

    maybe this time workflow actually running  success

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.book(author, isbn, price, title, created_date, last_modified_date, version)
+VALUES ('James Clear', '1234567896', 8.9, 'Atomic Habits', '2023-08-18 10:00:53.910595',
+        '2023-08-18 10:00:53.910595', 1);
