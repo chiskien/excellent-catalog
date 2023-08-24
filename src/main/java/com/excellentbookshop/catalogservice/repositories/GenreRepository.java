027852fbfb884a036446a33efe31d786da13e307
ChisKien <chiskien03214@gmail.com>
2023-08-24 08:02:27 +0700

    remove crudRepo in GenreRepo

--- src/main/java/com/excellentbookshop/catalogservice/repositories/GenreRepository.java
+++ src/main/java/com/excellentbookshop/catalogservice/repositories/GenreRepository.java
@@ -1,13 +1,12 @@
 package com.excellentbookshop.catalogservice.repositories;
 
 import com.excellentbookshop.catalogservice.models.Genre;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface GenreRepository extends CrudRepository {
+public interface GenreRepository {
 
     Optional<Genre> findGenreById(Genre genre);
 }
