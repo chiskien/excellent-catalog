8f26d25ac5e25849329f7d9c94557479cb70b868
ChisKien <chiskien03214@gmail.com>
2023-08-24 07:59:18 +0700

    forgot to run docker engine

--- src/main/java/com/excellentbookshop/catalogservice/config/DataConfig.java
+++ src/main/java/com/excellentbookshop/catalogservice/config/DataConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 @Configuration
+
 @EnableJdbcAuditing
 public class DataConfig {
 }
